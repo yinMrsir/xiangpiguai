7337e3e8e736a0c7fad30bdd14c3741b88757454
殷春雨 <yinchunyu@jf.com>
2019-02-13 16:19:45 +0800

    '修改方向'

--- src/script/GameView.js
+++ src/script/GameView.js
@@ -9,7 +9,7 @@ export default class Game extends Laya.Script{
     onEnable() {
         this.wallWidth = 45    // 墙宽
         this.jump = 30         // 橡皮怪跳跃的高度
-        this.DiciHeight = 93   // 地刺高度
+        this.DiciHeight = 140   // 地刺高度
         this.gameOver = false
 
         let dataSource = []
@@ -70,7 +70,7 @@ export default class Game extends Laya.Script{
         let n = 0
         this.Dicis.forEach((v, index) => {
             // 要移动的高度为 自身y点坐标 减去 自身的高度
-            let y = v.y - v.height
+            let y = v.y + v.height
             this.Tween.to(v, { y : y}, 100, null, Laya.Handler.create(this, () => {
                 n++
                 if (n === this.Dicis.length) {
@@ -94,8 +94,8 @@ export default class Game extends Laya.Script{
 
     // 检测第一个地刺是否超出页面
     checkDici() {
-        if (this.Dicis[0].y - this.Dicis[0].height <= 0) {
-            this.createDici(this.Dicis[this.Dicis.length -1].y)
+        if (this.Dicis[0].y >= Laya.stage.height) {
+            this.createDici(this.Dicis[this.Dicis.length - 1].y)
             this.Dicis[0].removeSelf()
             this.Dicis.shift()
         }
@@ -103,8 +103,8 @@ export default class Game extends Laya.Script{
 
     // 首次创建地刺
     oneCreateDici() {
-        for(let i = 0; i < 15; i++) {
-            this.createDici(i * this.DiciHeight + (this.DiciHeight + Math.ceil(this.DiciHeight/2)) + 100)
+        for(let i = 0; i < 8; i++) {
+            this.createDici(Laya.stage.height - (i * this.DiciHeight *2 + (this.DiciHeight + Math.ceil(this.DiciHeight/2)) + 100))
         }
     }
 
@@ -114,9 +114,9 @@ export default class Game extends Laya.Script{
         let direction = Math.random() < 0.5 ? 1 : 2
         let Dici = Laya.Pool.getItemByCreateFun('dici', this.Dici.create, this.Dici)
         if (direction === 1) {
-            Dici.pos(this.wallWidth + Dici.width/2, y)
+            Dici.pos(this.wallWidth + Dici.width/2, y - Dici.height)
         } else {
-            Dici.pos(Laya.stage.width-Dici.width - this.wallWidth + Dici.width/2, y)
+            Dici.pos(Laya.stage.width-Dici.width - this.wallWidth + Dici.width/2, y - Dici.height)
             Dici.rotation = '180'
         }
         this._gameBox.addChild(Dici)
@@ -126,7 +126,7 @@ export default class Game extends Laya.Script{
     // 创建橡皮鬼
     createNormal() {
         this.Normal = Laya.Pool.getItemByCreateFun('normal', this.Normal.create, this.Normal)
-        this.Normal.pos(this.wallWidth + this.Normal.width/2, this.Normal.height/2 + 102)
+        this.Normal.pos(this.wallWidth + this.Normal.width/2, Laya.stage.height - (this.Normal.height/2 + 102))
         this._gameBox.addChild(this.Normal)
     }
 }
